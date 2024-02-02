cbc1604a93ddf2e1b713e2f6506f9db87d760fee
chdonncha <chdonncha@gmail.com>
2024-02-02 22:28:01 +0000

    Convert project to use ts & scss

---
+++ ascii-tree-builder-v2/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+// import TreeBuilder from './components/TreeBuilder';
+// import Instructions from './components/Instructions';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h3>ASCII Tree Builder</h3>
+        </header>
+        {/*<Instructions />*/}
+        <div className="tree-container">
+          {/*<TreeBuilder />*/}
+        </div>
+      </div>
+  );
+}
+
+export default App;
